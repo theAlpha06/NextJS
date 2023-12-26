b473f5b32867449a53b21a6eaf62747c224666bd
Shubham <shubhamsinghmau2@gmail.com>
2023-12-26 13:27:36 +0530

    routes

--- React-Refresher/react-crash-course/src/main.jsx
+++ React-Refresher/react-crash-course/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
+import NewPost from './components/NewPost.jsx'
+import RootLayout from './routes/RootLayout.jsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <RootLayout />, children: [
+    { path: '/', element: <App /> },
+    { path: '/create-post', element: <NewPost />}
+  ]},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
