76af92d5384544b77ede53d4494debd374822b8a
Shubham <shubhamsinghmau2@gmail.com>
2023-12-26 13:44:50 +0530

    routes

--- React-Refresher/react-crash-course/src/components/Modal.jsx
+++ React-Refresher/react-crash-course/src/components/Modal.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import classes from './Modal.module.css';
 
-function Modal(props) {
+function Modal({ children }) {
+  const navigate = useNavigate();
+  function closeHandler() {
+    navigate('..')
+  }
+
   return (
     <>
-      <div className={classes.backdrop} onClick={props.onClose}/>
-      <dialog open className={classes.modal}>{props.children}</dialog>
+      <div className={classes.backdrop} onClick={closeHandler} />
+      <dialog open className={classes.modal}>{children}</dialog>
     </>
   );
 }
--- React-Refresher/react-crash-course/src/routes/NewPost.jsx
+++ React-Refresher/react-crash-course/src/routes/NewPost.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 import classes from './NewPost.module.css';
 import Modal from '../components/Modal';
 
@@ -37,9 +37,9 @@ function NewPost({ onCancel, onAddPost }) {
           <input type="text" id="name" required onChange={authorChangeHandler} />
         </p>
         <p className={classes.actions}>
-          <button type="button" onClick={onCancel}>
+          <Link to="/" type='button'>
             Cancel
-          </button>
+          </Link>
           <button>Submit</button>
         </p>
       </form>
