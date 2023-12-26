36db080634a4f2d77d90465af3b056928b4fad98
Shubham <shubhamsinghmau2@gmail.com>
2023-12-26 13:10:24 +0530

    post add and fetch

--- React-Refresher/react-crash-course/src/components/PostList.jsx
+++ React-Refresher/react-crash-course/src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Post from './Post';
 import NewPost from './NewPost';
@@ -8,7 +8,33 @@ import classes from './PostList.module.css';
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
+  //This will cause an infinite loop
+
+  // fetch('http://localhost:8080/posts')
+  // .then(response => response.json())
+  // .then(data => {
+  //   setPosts(data.posts);
+  // });
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch('http://localhost:8080/posts')
+      const resData =  await response.json();
+      setPosts(resData.posts);
+    }
+
+    fetchPosts();
+  }, [])
+
+
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
@@ -19,14 +45,14 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {posts && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {posts && posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
