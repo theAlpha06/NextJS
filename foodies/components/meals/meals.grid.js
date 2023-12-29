daff1a1aee12e9c20d277a41bfc0b499745d39c1
Shubham <shubhamsinghmau2@gmail.com>
2023-12-30 01:32:40 +0530

    minor fix

--- foodies/components/meals/meals.grid.js
+++ foodies/components/meals/meals.grid.js
@@ -2,8 +2,9 @@ import MealItem from './meal-item'
 import classes from './meals-grid.module.css'
 
 export default function MealsGrid({meals}) {
-  return <ul>
-    {meals.map(meal => <li key={meal.id} className={classes.meals}>
+
+  return <ul className={classes.meals}>
+    {meals.map(meal => <li key={meal.id}>
       <MealItem {...meal} />
     </li>)}
   </ul>
