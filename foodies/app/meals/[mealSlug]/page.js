028aa8b2244c46eea82e8b94c5d3a17238970bb8
Shubham <shubhamsinghmau2@gmail.com>
2023-12-30 00:59:59 +0530

    meal details

--- foodies/app/meals/[mealSlug]/page.js
+++ foodies/app/meals/[mealSlug]/page.js
@@ -1,7 +1,32 @@
-export default function MealDetailsPage() {
+import Image from 'next/image'
+import classes from './page.module.css'
+import { getMeal } from '@/lib/meals'
+
+export default  function MealDetailsPage({params}) {
+
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\\n/g, '\n');
+
   return (
-    <main>
-      <h1>Meals Slug</h1>
+    <>
+    <header className={classes.header}>
+      <div className={classes.image}>
+        <Image src={meal.image} fill alt='.'/>
+      </div>
+      <div className={classes.headerText}>
+        <h1>{meal.title}</h1>
+        <p className={classes.creator}>
+           by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+        </p>
+        <p className={classes.summary}>{meal.summary}</p>
+      </div>
+    </header>
+    <main className={classes.main}>
+      <p className={classes.instructions} dangerouslySetInnerHTML={{
+        __html: meal.instructions,
+      }}></p>
     </main>
+    </>
   )
 }
