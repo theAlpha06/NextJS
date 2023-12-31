33779a01a24ca9f13a439045f96900bd6189d13d
Shubham <shubhamsinghmau2@gmail.com>
2023-12-31 14:19:44 +0530

    dynamic routes value

--- page_route/pages/portfolio/[projectid].js
+++ page_route/pages/portfolio/[projectid].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 function PortfolioProjectPage() {
+
+  const router = useRouter();
+
+  console.log(router.pathname)
+  console.log(router.query)
+
   return (
     <div>PortfolioProjectPage</div>
   )
