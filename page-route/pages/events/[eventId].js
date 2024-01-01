d8a32175cc229cd00cc32cb03942cfb389e7484e
Shubham <shubhamsinghmau2@gmail.com>
2024-01-01 17:31:25 +0530

    event page

--- page-route/pages/events/[eventId].js
+++ page-route/pages/events/[eventId].js
@@ -1,6 +1,27 @@
+import { useRouter } from "next/router"
+import { getEventById } from "../../data/dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 function EventDetailPage() {
+  const router = useRouter();
+
+  const event = getEventById(router.query.eventId);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
+
   return (
-    <div>EventDetailPage</div>
+    <Fragment>
+      <EventSummary title={event.title}/>
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   )
 }
 
