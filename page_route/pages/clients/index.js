8bbf0ec951b5aca8d5b8fae3fc206bea3c47c994
Shubham <shubhamsinghmau2@gmail.com>
2024-01-01 13:41:29 +0530

    different way of setting link href

--- page_route/pages/clients/index.js
+++ page_route/pages/clients/index.js
@@ -1,6 +1,27 @@
+import Link from "next/link"
+
 function ClientsPage() {
+
+  const clients = [
+    { id: 'max', name: 'Maximilian' },
+    { id: 'manu', name: 'Manuel' },
+  ]
   return (
-    <div>ClientsPage</div>
+    <>
+      <div>ClientsPage</div>
+      <ul>
+        {clients.map(client => (
+          <li key={client.id}>
+            <Link href={{
+              pathname: '/clients/[id]',
+              query: { id: client.id }
+            }}>
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
