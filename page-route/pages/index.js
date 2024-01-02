d620e6419f364a22bacea6ecd9b2e8a88d2ad0a5
Shubham <shubhamsinghmau2@gmail.com>
2024-01-02 14:20:47 +0530

    rename folders for consistency

--- page-route/pages/index.js
+++ page-route/pages/index.js
@@ -1,11 +1,32 @@
-import EventList from "../components/events/event-list"
-import { getFeaturedEvents } from '../data/dummy-data';
+import Link from "next/link"
 
 function HomePage() {
-  const featureEvents = getFeaturedEvents();
   return (
     <div>
-      <EventList items={featureEvents}/>
+      <h1>Next.js</h1>
+      <ul>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+        <li>
+          <Link href="/portfolio/1">Portfolio Project 1</Link>
+        </li>
+        <li>
+          <Link href="/portfolio/2">Portfolio Project 2</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link href="/blog/first-blog-post/something/nothing">First Blog Post</Link>
+        </li>
+      </ul>
     </div>
   )
 }
