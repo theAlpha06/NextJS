d3544e2e7254ac4d447277bb45af977e366c531f
Shubham <shubhamsinghmau2@gmail.com>
2024-01-02 13:49:33 +0530

    filtered events page

--- page-route/pages/events/[...slug].js
+++ page-route/pages/events/[...slug].js
@@ -1,6 +1,49 @@
+import { useRouter } from "next/router";
+
+import { getFilteredEvents } from "../../data/dummy-data";
+import EventList from "../../components/events/event-list";
+
+
 function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>
+  }
+
+  const [filteredYear, filteredMonth] = filterData; 
+  
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) || 
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>
+  }
+
+
+
   return (
-    <div>FilteredEventsPage</div>
+    <div>
+      <EventList items={filteredEvents} />
+    </div>
   )
 }
 
