ec9fb2cbf6896ba01dfb952d19c1aa5d9d218905
Shubham <shubhamsinghmau2@gmail.com>
2024-01-02 15:00:43 +0530

    some more options

--- data-fetching/pages/index.js
+++ data-fetching/pages/index.js
@@ -2,7 +2,7 @@ import fs from 'fs/promises';
 import path from 'path';
 
 function HomePage(props) {
-  const {products} = props;
+  const { products } = props;
 
   //? React way of fetching data from backend
   // const [product, setProduct] = useState([]);
@@ -21,9 +21,9 @@ function HomePage(props) {
 
   return (
     <ul>
-     {products.map((product) => (
-       <li key={product.id}>{product.title}</li>
-     ))}
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 }
@@ -34,11 +34,33 @@ export async function getStaticProps() {
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  /*
+
+  if (!data) {
+    return { notFound: true }
+  }
+
+  if (data.products.length === 0) {
+    return { redirect: { destination: '/no-data' } }
+  }
+
+  */
+
   return {
     props: {
       products: data.products,
     },
-    revalidate: 10, // regenerate every 10 seconds until a new request comes in less than 10 seconds
+    revalidate: 10, // regenerate every 10 seconds until a new request comes in less than 10 seconds(in development mode it will always regenerate)
+
+    /*
+
+    notFound: true // if true, it will show 404 page if the page is not found
+
+    redirect: {
+      destination: '/no-data' // redirect to this page if the page is not found
+    }
+
+    */
   }
 }
 
