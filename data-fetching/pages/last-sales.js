081466e480687d1a998f4ac46f9e8cc302f7be06
Shubham <shubhamsinghmau2@gmail.com>
2024-01-03 22:38:16 +0530

    swr

--- data-fetching/pages/last-sales.js
+++ data-fetching/pages/last-sales.js
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react"
+import useSWR from "swr";
 
 
 function LastSalesPage() {
 
   const [sales, setSales] = useState()
 
+  const { data, error } = useSWR('https://nextjs-course-3b7e2-default-rtdb.firebaseio.com/sales.json');
+
+
+
   useEffect(() => {
-    fetch('https://nextjs-course-3b7e2-default-rtdb.firebaseio.com/sales.json')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
+    if (data) {
       const transformedSales = [];
 
       for (const key in data) {
@@ -21,21 +22,46 @@ function LastSalesPage() {
         });
       }
       setSales(transformedSales);
-    });
-  }, [])
+    }
+  }, [data]);
 
-  if(!sales) {
+
+
+  // useEffect(() => {
+  //   fetch('https://nextjs-course-3b7e2-default-rtdb.firebaseio.com/sales.json')
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     const transformedSales = [];
+
+  //     for (const key in data) {
+  //       transformedSales.push({
+  //         id: key,
+  //         username: data[key].username,
+  //         volume: data[key].volume,
+  //       });
+  //     }
+  //     setSales(transformedSales);
+  //   });
+  // }, [])
+
+  if (error) {
+    return <p>Failed to load</p>
+  }
+
+  if (!data || !sales) {
     return <p>Loading...</p>
   }
 
-  
+
 
   return (
-   <ul>
-    {sales?.map((sale) => (
-      <li key={sale.id}>{sale.username} - ${sale.volume}</li>
-    ))}
-   </ul>
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>{sale.username} - ${sale.volume}</li>
+      ))}
+    </ul>
   )
 }
 
