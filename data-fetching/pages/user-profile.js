340cf06f40d30d1847d10d82e93e97aee210401d
Shubham <shubhamsinghmau2@gmail.com>
2024-01-03 11:47:57 +0530

    difference with getStaticProps()

--- data-fetching/pages/user-profile.js
+++ data-fetching/pages/user-profile.js
@@ -8,7 +8,13 @@ function UserProfilePage(props) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
+
+  const { params, req, res } = context;
+  //Unlike getStaticProps() we also get the request and response objects here
+  //We can use them to set cookies for example
+  //req and res are the official NodeJS request and response objects respectively
+
   return {
     props: {
       username: 'Sunny'
