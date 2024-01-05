5eab2ae9349b5b07719e87013f3fae610d8852ea
Shubham <shubhamsinghmau2@gmail.com>
2024-01-05 22:52:27 +0530

    use getStaticProps() to fetch events data

---
+++ page rerendering/helpers/api-util.js
@@ -0,0 +1,13 @@
+export async function getAllEvents() {
+  const response = await fetch('https://nextjs-19472-default-rtdb.firebaseio.com/.json');
+  const data = await response.json();
+  return data;
+}
+
+getAllEvents();
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+
+  return allEvents.filter((event) => event.isFeatured);
+}
---
+++ page rerendering/pages/index.js
@@ -0,0 +1,22 @@
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+
+function HomePage({events}) {
+
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    }
+  }
+}
+
+export default HomePage;
