2e15281563607bdf912a277467d9db722b703c81
Shubham <shubhamsinghmau2@gmail.com>
2024-01-06 01:08:28 +0530

    meta

--- page rerendering/pages/_app.js
+++ page rerendering/pages/_app.js
@@ -6,8 +6,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <title>Filtered Events</title>
-        <meta name='description' content={`All events for ${numMonth}/${numYear}`} />
+        <title>Next Events</title>
+        <meta name='description' content={`Upcomming Events`} />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />  {/*this is where the page content is rendered */}
