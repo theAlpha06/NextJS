25224f22b6880663f45c015431bb7ae200ad5c85
Shubham <shubhamsinghmau2@gmail.com>
2024-01-07 18:11:46 +0530

    newsletter api

---
+++ api-routes-practise/components/input/newsletter-registration.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import classes from './newsletter-registration.module.css';
+
+function NewsletterRegistration() {
+
+  const [email, setEmail] = useState()
+
+  async function registrationHandler(event) {
+    event.preventDefault();
+
+    const res = await fetch('/api/newsletter/register', {
+      method: 'POST',
+      body: JSON.stringify(email),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const resData  = await res.json();
+    console.log(resData);
+    // fetch user input (state or refs)
+    // optional: validate input
+    // send valid data to API
+  }
+
+  return (
+    <section className={classes.newsletter}>
+      <h2>Sign up to stay updated!</h2>
+      <form onSubmit={registrationHandler}>
+        <div className={classes.control}>
+          <input
+            type='email'
+            id='email'
+            placeholder='Your email'
+            aria-label='Your email'
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button type='submit'>Register</button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default NewsletterRegistration;
