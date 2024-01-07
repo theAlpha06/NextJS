5cd14897ee5fde17f9e2e88a386b18133e31a097
Shubham <shubhamsinghmau2@gmail.com>
2024-01-07 23:00:28 +0530

    notification ui

--- api-routes-practise/pages/api/newsletter/register.js
+++ api-routes-practise/pages/api/newsletter/register.js
@@ -1,8 +1,18 @@
+import { MongoClient } from 'mongodb';
+
 function handler(req, res) {
   if (req.method === 'POST') {
-    return res.status(201).json({
-      message: 'Successfully subscribed!'
-    })
+    MongoClient.connect('mongodb+srv://thealpha06:thealpha06@cluster0.6xsrrev.mongodb.net/', function (err, client) {
+      if (err) throw err;
+      const db = client.db('nextjs');
+      console.log('db')
+      db.collection("emails").insertOne(req.body, function (err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        client.close();
+      });
+    });
+    return res.status(201).json({ message: 'Subscribed!' });
   }
   return res.status(404).json({ message: 'Route not found!' });
 }
