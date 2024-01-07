fd1ed7e44a1758cc16fce4832d3683840447c5fb
Shubham <shubhamsinghmau2@gmail.com>
2024-01-07 23:12:26 +0530

    use context to show notification

--- api-routes-practise/components/input/newsletter-registration.js
+++ api-routes-practise/components/input/newsletter-registration.js
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import classes from './newsletter-registration.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function NewsletterRegistration() {
 
-  const [email, setEmail] = useState()
+  const [email, setEmail] = useState();
+  const { showNotification } = useContext(NotificationContext);
 
   async function registrationHandler(event) {
     event.preventDefault();
+
+    showNotification({
+      title: 'Signing up...',
+      message: 'Registering for newsletter.',
+      status: 'pending'
+    });
 
     const res = await fetch('/api/newsletter/register', {
       method: 'POST',
@@ -15,8 +23,20 @@ function NewsletterRegistration() {
         'Content-Type': 'application/json'
       }
     })
-    const resData  = await res.json();
-    console.log(resData);
+    const resData = await res.json();
+    if(res.status === 201) {
+      showNotification({
+        title: 'Success!',
+        message: 'Successfully registered for newsletter.',
+        status: 'success'
+      });
+    } else {
+      showNotification({
+        title: 'Error!',
+        message: resData.message || 'Something went wrong.',
+        status: 'error'
+      });
+    }
   }
 
   return (
