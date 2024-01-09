c51aaa6139b1d54f9485a870dc963938864f4683
Shubham <shubhamsinghmau2@gmail.com>
2024-01-09 13:54:20 +0530

    rendering Next img in markdown

--- blog-website/components/posts/post-detail/post-content.js
+++ blog-website/components/posts/post-detail/post-content.js
@@ -1,14 +1,39 @@
+import Image from 'next/image';
 import classes from './post-content.module.css'
 import PostHeader from './post-header';
 import ReactMarkdown from 'react-markdown'
 
 
 function PostContent(props) {
+
   const {post} = props;
+
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   const imagePath = `/images/posts/${post.image}`;
   return <article className={classes.content}>
     <PostHeader title={post.title} image={imagePath} />
-    <ReactMarkdown>
+    <ReactMarkdown components={customRenderers}>
       {post.content}
     </ReactMarkdown>
   </article>
